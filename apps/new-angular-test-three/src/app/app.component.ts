a3c939c08cca5f38deeb31f97857bfb20f550a20
Charlie Greenman <charlie@razroo.com>
2023-08-22 00:39:13 -0700

    Create starter files for angular-16.1.0

---
+++ apps/new-angular-test-three/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'new-angular-test-three-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'new-angular-test-three';
+}
